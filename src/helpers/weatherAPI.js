4d6d942a211b853bbb997f2edf91beaabaf9391c
Ronaldo Cipriano <ronaldolopes14@gmail.com>
2023-05-25 16:37:12 -0300

    Requisito 1

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,9 +1,17 @@
-// Remova os comentários a medida que for implementando as funções
+const TOKEN = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqu
+export const searchCities = (term) => {
+  fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      if (data.length === 0) {
+        window.alert('Nenhuma cidade encontrada');
+      }
+      return data;
+    });
 };
 
-export const getWeatherByCity = (/* cityURL */) => {
+export const getWeatherByCity = () => {
 //   seu código aqui
 };
